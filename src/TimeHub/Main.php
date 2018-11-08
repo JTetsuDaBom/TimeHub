3dbda3be9829beeb94a13e4d0167a5b6e2bf0115
MegaTheDev <40536128+XxMegaIS123xX@users.noreply.github.com>
2018-11-08 19:59:16 +0800

    Create Main.php

---
+++ src/TimeHub/Main.php
@@ -0,0 +1,11 @@
+<?php
+use pocketmine\Server;
+use pocketmne\Player;
+use pocketmine\utils\Config;
+use pocketmine\events\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class Main extends PluginBase implements Listener{
+}
