bca205937abdabd2dc64dc82e841e5d3fe1a0f26
MegaTheDev <40536128+XxMegaIS123xX@users.noreply.github.com>
2018-11-09 20:29:40 +0800

    Update Main.php
    disable & enable

--- src/TimeHub/Main.php
+++ src/TimeHub/Main.php
@@ -1,6 +1,6 @@
 <?php
 use pocketmine\Server;
-use pocketmne\Player;
+use pocketmine\Player;
 use pocketmine\utils\Config;
 use pocketmine\events\Listener;
 use pocketmine\plugin\PluginBase;
@@ -8,4 +8,14 @@ use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 
 class Main extends PluginBase implements Listener{
+  
+  public function onEnable(){
+    $this->getLogger()->info("T-Hub plugin enabled made by MegaTheDev");
+  }
+  public function onDisable(){
+    $this->getLogger()->info('T-Hub disabled restart the server to enable the plugin");
+  }
+  public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args){
+  
+  }
 }
